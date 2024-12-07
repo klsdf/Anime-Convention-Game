3392acf90586b6275791023335b0ccc6fe945354
Jmingxiao <Mingxiao_0719@outlook.com>
2024-12-07 17:47:54 +0800

    Add dotween animation and fix bg scrolling
    影响范围: start scene bug fix

--- Assets/Scripts/UI/ScrollImage.cs
+++ Assets/Scripts/UI/ScrollImage.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 //作者：闫辰祥
 //创建时间: 2024/8/13
 
@@ -9,39 +8,45 @@ public class ScrollImage : MonoBehaviour
     public float speed = 100f; // 滚动速度
     private RectTransform rectTransform;
     private float canvasWidth;
-    private bool movingLeft = false;
+
 
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
-        canvasWidth = rectTransform.parent.GetComponent<RectTransform>().rect.width;
-    }
-
-    void Update()
-    {
         float imageWidth = rectTransform.rect.width;
-
-        if (movingLeft)
-        {
-            // 向左移动
-            rectTransform.anchoredPosition += Vector2.left * speed * Time.deltaTime;
-
-            // 如果图片移动到最左边，改为向右移动
-            if (rectTransform.anchoredPosition.x <= -20f) // 可以根据需要调整这个值
-            {
-                movingLeft = false;
-            }
-        }
-        else
-        {
-            // 向右移动
-            rectTransform.anchoredPosition += Vector2.right * speed * Time.deltaTime;
-
-            // 如果图片移动到最右边，改为向左移动
-            if (rectTransform.anchoredPosition.x >= 20f) // 可以根据需要调整这个值
-            {
-                movingLeft = true;
-            }
-        }
+        canvasWidth = rectTransform.parent.GetComponent<RectTransform>().rect.width;
+        
+        var scrollAnim = DOTween.To(() => rectTransform.anchoredPosition, x => rectTransform.anchoredPosition = x, 
+        rectTransform.anchoredPosition-new Vector2(imageWidth-canvasWidth, 0), 10/speed).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+        scrollAnim.Restart();
     }
+
+    //private bool movingLeft = false;
+    // void Update()
+    // {
+    //     float imageWidth = rectTransform.rect.width;
+
+    //     if (movingLeft)
+    //     {
+    //         // 向左移动
+    //         rectTransform.anchoredPosition += Vector2.left * speed * Time.deltaTime;
+
+    //         // 如果图片移动到最左边，改为向右移动
+    //         if (rectTransform.anchoredPosition.x <= -20f) // 可以根据需要调整这个值
+    //         {
+    //             movingLeft = false;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         // 向右移动
+    //         rectTransform.anchoredPosition += Vector2.right * speed * Time.deltaTime;
+
+    //         // 如果图片移动到最右边，改为向左移动
+    //         if (rectTransform.anchoredPosition.x >= 20f) // 可以根据需要调整这个值
+    //         {
+    //             movingLeft = true;
+    //         }
+    //     }
+    // }
 }
