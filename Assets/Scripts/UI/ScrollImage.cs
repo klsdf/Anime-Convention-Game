c8f9281145653634031ad7b06effdb5f56d249d2
Jmingxiao <Mingxiao_0719@outlook.com>
2024-12-15 23:08:29 +0800

    fix#雨滴fx在bg前显示（暂时的解决方案后期有时间优化）调整下main menu setting的ui参数

--- Assets/Scripts/UI/ScrollImage.cs
+++ Assets/Scripts/UI/ScrollImage.cs
@@ -24,33 +24,23 @@ public class ScrollImage : MonoBehaviour
         scrollAnim.Restart();
     }
 
-    void Update()
-    {
-        float imageWidth = rectTransform.rect.width;
-    //private bool movingLeft = false;
     // void Update()
     // {
     //     float imageWidth = rectTransform.rect.width;
+    // //private bool movingLeft = false;
+    // // void Update()
+    // // {
+    // //     float imageWidth = rectTransform.rect.width;
 
-        if (movingLeft)
-        {
-            // 向左移动
-            rectTransform.anchoredPosition += Vector2.left * speed * Time.deltaTime;
     //     if (movingLeft)
     //     {
     //         // 向左移动
     //         rectTransform.anchoredPosition += Vector2.left * speed * Time.deltaTime;
+    // //     if (movingLeft)
+    // //     {
+    // //         // 向左移动
+    // //         rectTransform.anchoredPosition += Vector2.left * speed * Time.deltaTime;
 
-            // 如果图片移动到最左边，改为向右移动
-            if (rectTransform.anchoredPosition.x <= -20f) // 可以根据需要调整这个值
-            {
-                movingLeft = false;
-            }
-        }
-        else
-        {
-            // 向右移动
-            rectTransform.anchoredPosition += Vector2.right * speed * Time.deltaTime;
     //         // 如果图片移动到最左边，改为向右移动
     //         if (rectTransform.anchoredPosition.x <= -20f) // 可以根据需要调整这个值
     //         {
@@ -61,14 +51,17 @@ public class ScrollImage : MonoBehaviour
     //     {
     //         // 向右移动
     //         rectTransform.anchoredPosition += Vector2.right * speed * Time.deltaTime;
+    // //         // 如果图片移动到最左边，改为向右移动
+    // //         if (rectTransform.anchoredPosition.x <= -20f) // 可以根据需要调整这个值
+    // //         {
+    // //             movingLeft = false;
+    // //         }
+    // //     }
+    // //     else
+    // //     {
+    // //         // 向右移动
+    // //         rectTransform.anchoredPosition += Vector2.right * speed * Time.deltaTime;
 
-            // 如果图片移动到最右边，改为向左移动
-            if (rectTransform.anchoredPosition.x >= 20f) // 可以根据需要调整这个值
-            {
-                movingLeft = true;
-            }
-        }
-    }
     //         // 如果图片移动到最右边，改为向左移动
     //         if (rectTransform.anchoredPosition.x >= 20f) // 可以根据需要调整这个值
     //         {
@@ -76,4 +69,11 @@ public class ScrollImage : MonoBehaviour
     //         }
     //     }
     // }
+    // //         // 如果图片移动到最右边，改为向左移动
+    // //         if (rectTransform.anchoredPosition.x >= 20f) // 可以根据需要调整这个值
+    // //         {
+    // //             movingLeft = true;
+    // //         }
+    // //     }
+    // // }
 }
